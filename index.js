2b38859ea63d049a3c5e5855aed35b42e308b759
mohamedktbffh@gmail.com <mohamedktbffh@gmail.com>
2017-10-22 18:34:21 +0100

    server-setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+/*const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+  if (err) {
+    console.log('Could Not Connect: ',err);
+  } else {
+    console.log('Connecting to db: ',config.db);
+  }
+});*/
+
+app.use(express.static(__dirname + '/client/dist' ))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+});
+
+app.listen(8080, () => {
+  console.log('Listenning')
+});
